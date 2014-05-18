1cee8e45afe5f1733ce07b4c91a93108b3a468b6
Tom Yu <tom.yu@burstingbrains.com>
2014-05-18 15:59:26 -0500

    Functionality is complete

--- Assets/_Bursting Brains/Scripts/Util/UtilLogger.cs
+++ Assets/_Bursting Brains/Scripts/Util/UtilLogger.cs
@@ -4,10 +4,10 @@ using System.Collections;
 public static class UtilLogger {
 
 	public static void LogInfo(string tag, string message) {
-		Debug.Log(tag + " " + message);
+		//Debug.Log(tag + " " + message);
 	}
 
 	public static void LogInfo(string tag, char message) {
-		Debug.Log(tag + " " + message);
+		//Debug.Log(tag + " " + message);
 	}
 }

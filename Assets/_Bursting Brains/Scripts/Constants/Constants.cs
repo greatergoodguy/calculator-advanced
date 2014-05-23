bf29004e3f1ab0c6126a37986922c6e1d3933912
Tom Yu <tom.yu@burstingbrains.com>
2014-05-22 21:19:32 -0500

    Imported Font and formatted display number

---
+++ Assets/_Bursting Brains/Scripts/Constants/Constants.cs
@@ -0,0 +1,6 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class Constants {
+	public static string DISPLAY_STRING_FORMATTER = "G12";
+}
--- Assets/_Bursting Brains/Scripts/Controller/CtrlDisplay.cs
+++ Assets/_Bursting Brains/Scripts/Controller/CtrlDisplay.cs
@@ -3,14 +3,21 @@ using System.Collections;
 
 public class CtrlDisplay : Ctrl_Base {
 
-	tk2dTextMesh tmText;
+	tk2dTextMesh text2D;
+	TextMesh text3D;
 
 	void Awake() {
-		tmText = transform.FindChild("Text").GetComponent<tk2dTextMesh>();
+		text2D = transform.FindChild("Text 2D").GetComponent<tk2dTextMesh>();
+		text3D = transform.FindChild("Text 3D").GetComponent<TextMesh>();
 	}
 
-	public void DisplayString(string text) {
-		tmText.text = text;
-		tmText.Commit();
+	public void DisplayString(string numberAsString) {
+		double number = double.Parse(numberAsString);
+		numberAsString = number.ToString(Constants.DISPLAY_STRING_FORMATTER);
+
+		text2D.text = numberAsString;
+		text2D.Commit();
+
+		text3D.text = numberAsString;
 	}
 }
